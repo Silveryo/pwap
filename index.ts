fc1a7f5a6fe022bd33430ccf2b8c4643f61d89e8
Zdeněk Odehnal <odehnal.business@gmail.com>
2022-04-19 19:38:32 +0200

    Removed local port cause still erroring

--- index.ts
+++ index.ts
@@ -3,9 +3,8 @@ import bootstrap from './App/bootstrap';
 
 bootstrap();
 
-const PORT = 2222;
-const server =app.listen((PORT || process.env.PORT), () => {
-    console.log(`App running on localhost:${PORT}`);
+const server =app.listen((process.env.PORT), () => {
+    console.log(`App running!`);
 });
 
 export default server;
