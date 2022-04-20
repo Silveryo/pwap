d931f689ae712ea0af45806fb02707ee10d61cb9
Zdeněk Odehnal <odehnal.business@gmail.com>
2022-04-20 16:21:34 +0200

    Big fix

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,20 @@
 import { DataSource } from "typeorm";
+import { Client } from "pg";
+
+const client = new Client({
+    connectionString:process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: client.host,
+    port: client.port,
+    username: client.user,
+    password: client.password,
+    database: client.database,
     entities: ["./App/Entities/*"],
     synchronize: true,
 });
