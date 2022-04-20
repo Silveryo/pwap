503172400418be2066d19a0fb9e13d4025240b10
Zdeněk Odehnal <odehnal.business@gmail.com>
2022-04-20 07:34:52 +0200

    heroku

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: 'ec2-23-20-224-166.compute-1.amazonaws.com',
     port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "pwap",
+    username: 'rjtxrtnkinmdma',
+    password: '1c84e318c377cc01808708bf98a3c55a63132761dd207e2e7d54ef02b5bc71ca',
+    database: 'def2kubdq0f9ov',
     entities: ["./App/Entities/*"],
     synchronize: true,
 });
